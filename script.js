352cbe7f18fe1e424338bc23d519de204bd48f17
CCTheGoat <carv0009@student.acitech.org>
2025-03-31 08:39:20 -0400

    final submission

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 
 //tells the computer all possible choices
 const choices = ['rock', 'paper', 'scissors', 'coffeemaker','inspiringcatposter'];
+//names all the id's for mycode
 const playerChoice = document.getElementById("playerChoice");
 const computerChoice = document.getElementById("computerChoice");
 const resultDisplay = document.getElementById("resultDisplay");
 const playerScoreDisplay = document.getElementById("playerScoreDisplay");
 const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 const resetButton = document.getElementById("Reset");
+//starts the score at 0
 let playerScore = 0;
 let computerScore = 0;
-
+//all elemnts of the game are wrapped in here
 function playGame(playerChoice) {
-
+//tells the computer how many choices there are and lets the result be show in text
   const computerChoice = choices[Math.floor(Math.random() * 5)];
   let result = "";
-
+//compares the computer choice to the player choice and decides who wins/loses also if it ends in a tie
   if(playerChoice === computerChoice) {
     result = "IT'S A TIE";
   }
@@ -37,13 +39,13 @@ function playGame(playerChoice) {
         break;
     }
   }
-
+//displays the you win/lose text depending on the outcomes
   playerDisplay.textContent = `PLAYER: ${playerChoice}`;
   computerDisplay.textContent = `Computer: ${computerChoice}`;
   resultDisplay.textContent = result;
 
   resultDisplay.classList.remove("greenText", "redText");
-
+//improves the score of both the computer and the player by 1 if they win, also adds color to the scores and you win/lose text
   switch(result){
     case "You Win":
         resultDisplay.classList.add("greenText");
@@ -56,6 +58,7 @@ function playGame(playerChoice) {
         computerScoreDisplay.textContent = computerScore;  
         break;
   }
+  //my reset function to restart the game at 0 
   function resetGame() {
     playerScore = 0;
     computerScore = 0;
